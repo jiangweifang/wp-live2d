9919786cafb81daef2a6838c47abd4763d219bf2
jiangweifang <38683169+jiangweifang@users.noreply.github.com>
2023-02-07 23:56:16 +0800

    Update wordpress-live2d.php

--- wordpress-live2d.php
+++ wordpress-live2d.php
@@ -3,7 +3,7 @@
  * Plugin Name: Live 2D
  * Plugin URI: https://blog.csdn.net/jiangweifang
  * Description: 看板娘插件
- * Version: 1.8.1
+ * Version: 1.8.2
  * Requires PHP: 7.4
  * Author: Weifang Chiang
  * Author URI: https://github.com/jiangweifang/wp-live2d
